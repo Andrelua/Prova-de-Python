13eedb7e9ae9661a50a669dc10083a0127c962f2
André Luan <Andreluan176@gmail.com>
2020-12-17 19:36:36 -0300

    Questão 3

--- Q3.py
+++ Q3.py
@@ -6,3 +6,19 @@ Açúcar - corredor 3, Café- corredor 4, Molho de tomate -corredor 5, Macarrão
 O funcionário precisa encontrar em qual corredor está o produto para ajudar o cliente em sua compra. 
 Use tupla para criar esse programa. 
 """
+
+produtos = (["Arroz", 1],["Feijão", 1],["Óleo de soja", 2],["Sal", 3],["Açúcar", 3],["Café", 4],["Molho de tomate", 5], ["Macarrão", 6])
+
+while True:
+    print("BEM-VINDO(A) AO SISTEMA DO SUPERMERCADO")
+    corredor = int(input("Qual corredor deseja pesquisar ?"))
+    print("Os produtos desse corredor são:")
+    for i in range(len(produtos)):
+        if corredor == produtos[i][1]:
+            print(f"Produto: {produtos[i][0]}.")
+    
+    saida = input("Mais alguma coisa (Y/N)? ")
+    if saida == "N":
+        break
+
+print("Até a próxima, volte sempre.")
