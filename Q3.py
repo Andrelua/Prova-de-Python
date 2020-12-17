30bef2352ef629b76a3360cc1032243e0be9b980
André Luan <Andreluan176@gmail.com>
2020-12-17 19:00:04 -0300

    Questão 1

---
+++ Q3.py
@@ -0,0 +1,8 @@
+"""
+[Questão criada por Myllena Laís (adaptada)] Um novo funcionário de supermercado quer saber em 
+qual corredor se encontra um produto. No sistema existem os seguintes produtos: 
+Arroz - corredor 1, Feijão - corredor 1, Óleo de soja - corredor 2, Sal - corredor 3, 
+Açúcar - corredor 3, Café- corredor 4, Molho de tomate -corredor 5, Macarrão- corredor 6. 
+O funcionário precisa encontrar em qual corredor está o produto para ajudar o cliente em sua compra. 
+Use tupla para criar esse programa. 
+"""
